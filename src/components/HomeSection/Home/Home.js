7f9da98595f4da5563afe18778afc14e6a2e3938
ferdouse1995 <ferdousebinteasad@gmail.com>
2022-11-04 09:04:19 -0700

    change

--- src/components/HomeSection/Home/Home.js
+++ src/components/HomeSection/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import Crrrunchtime from '../Crrrunchtime/Crrrunchtime';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
-            
+            <Crrrunchtime/>
         </div>
     );
 };
