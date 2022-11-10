d7934201ba86be9d6a67326397456af2715f0bf8
ferdouse1995 <ferdousebinteasad@gmail.com>
2022-11-10 06:14:18 -0800

    remove

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 import {  Route, Routes } from "react-router-dom";
 import NavBar from "./components/common/NavBar/NavBar";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.min.js";
 import ShowCase from "./components/ShowCase/ShowCase";
-// import Paralax from "./components/Parallax/Paralax";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/HomeSection/Home/Home";
 import Testimonial from "./components/HomeSection/Testimonial/Testimonial";
